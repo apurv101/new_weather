69b739ec2351a47de46efd60e3eebce899330ed9
APURV <apoorvagarwal00@gmail.com>
2023-04-11 19:35:09 +0530

    Add current conditions feature

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -4,10 +4,47 @@ class WeatherController < ApplicationController
   end
 
   def forecast
-    api_key = Rails.application.credentials.open_weather_map[:api_key]
+    api_key = '23fc32952da3ac8bcddd725fcfff3df6'
+    # api_key = Rails.application.credentials.open_weather_map[:api_key]
     service = OpenWeatherMap::Forecast.new(api_key)
     response = service.forecast(params[:city])
+
+
+    @current = service.current(params[:city])
+
     @forecast = response.parsed_response['list']
+
+    json_response = response.parsed_response['list']
+
+
+
+    # group temperature data by day
+    temp_by_day = {}
+    json_response.each do |data|
+      date = DateTime.parse(data["dt_txt"]).strftime("%Y-%m-%d")
+      temp = data["main"]["temp"]
+      if temp_by_day[date].nil?
+        temp_by_day[date] = [temp]
+      else
+        temp_by_day[date] << temp
+      end
+    end
+
+    # find max and min temperature for each day
+    temp_stats = {}
+    temp_by_day.each do |date, temps|
+      temp_stats[date] = {
+        max: sprintf("%.2f", temps.max - 273.15),
+        min: sprintf("%.2f", temps.min - 273.15)
+      }
+    end
+
+    @forecast_by_day = temp_stats
+
+    puts @forecast_by_day
+    puts '!'*100
+    puts @current
+
 
     if @forecast.present?
       render :forecast
@@ -15,5 +52,21 @@ class WeatherController < ApplicationController
       render :index
     end
   end
+
+  def current
+    # api_key = Rails.application.credentials.open_weather_map[:api_key]
+    api_key = '23fc32952da3ac8bcddd725fcfff3df6'
+    service = OpenWeatherMap::Current.new(api_key)
+    response = service.forecast(params[:city])
+    @current = response.parsed_response
+    print(@current)
+
+    if @current.present?
+      render :current
+    else
+      render :index
+    end
+  end
+
 end
 
--- app/services/open_weather_map/forecast.rb
+++ app/services/open_weather_map/forecast.rb
@@ -4,13 +4,20 @@ module OpenWeatherMap
   class Forecast
     BASE_URL = 'https://api.openweathermap.org/data/2.5'
 
+
     def initialize(api_key)
       @api_key = api_key
     end
 
     def forecast(city)
       @city = city
+      puts "#{BASE_URL}/weather?lat=#{lat}&lon=#{lon}&appid=#{@api_key}"
       HTTParty.get("#{BASE_URL}/forecast?lat=#{lat}&lon=#{lon}&appid=#{@api_key}")
+    end
+
+    def current(city)
+      @city = city
+      HTTParty.get("#{BASE_URL}/weather?lat=#{lat}&lon=#{lon}&appid=#{@api_key}")
     end
 
     private
